2d34a0b2c340e2419331b3fae2704d615a3ba922
platonovAlexey <kalyawe4ka@gmail.com>
2019-04-09 21:52:49 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template lang='pug'>
+  #app
+    h1 Init
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang='stylus'>
+#app
+  padding 30px
+  background #eaeaea
+  color black
+</style>
