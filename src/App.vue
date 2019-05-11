4d117b6613f172cba7ef6b1faeaebc7637313572
platonovAlexey <kalyawe4ka@gmail.com>
2019-05-11 19:06:25 +0300

    add style library, router, task page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template lang='pug'>
-  #app
-    h1 Init
+  .wrapper
+    header
+      .navbar
+        .container
+          .navbar-content
+            router-link.header-logo(to="/") Film and Series Library
+            .button-burger(
+              @click="menuShow = !menuShow"
+              :class="{ active: menuShow }"
+            )
+              span.line.line-1
+              span.line.line-2
+              span.line.line-3
+            .navbar-list__wrapper(
+              :class="{ active: menuShow }"
+            )
+              ul.navbar-list
+                li.navbar-item(
+                  v-for="link in linkMenu"
+                  :key="link.title"
+                  @click="menuShow = false"
+                )
+                  router-link.navbar-link(
+                    :to="`${link.url}`"
+                  ) {{ link.title }}
+    router-view
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      menuShow: false,
+      linkMenu: [
+        {
+          title: 'Home',
+          url: '/'
+        },
+        {
+          title: 'Login',
+          url: '/login'
+        },
+        {
+          title: 'Registration',
+          url: '/registration'
+        },
+      ]
+    }
+  }
 }
 </script>
 
 <style lang='stylus'>
-#app
-  padding 30px
-  background #eaeaea
-  color black
+@import './assets/stylus/main.styl'
 </style>
