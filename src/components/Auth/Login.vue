50ec7911fd68f2acc3eb0f8b4289bd339365cf59
platonovAlexey <kalyawe4ka@gmail.com>
2019-06-09 19:12:54 +0300

    add validate library to registration and login form

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -2,19 +2,120 @@
   .content-wrapper
     section
       .container
-        h1.ui-title-1 Login
+        .auth
+          .auth__banner
+            h1.ui-title-2 Hellow Banner
+          .auth__form
+            h2.ui-title-2 Login
+            form(@submit.prevent="onSubmit")
+              .form-item(:class="{ errorInput: $v.email.$error }")
+                input(
+                  type="email"
+                  Placeholder="Email"
+                  v-model="email"
+                  :class="{ error: $v.email.$error }"
+                  @change="$v.email.$touch()"
+                )
+                .error(v-if="!$v.email.required") Field is required
+                .error(v-if="!$v.email.email") Email is not correct
+              .form-item(:class="{ errorInput: $v.password.$error }")
+                input(
+                  type="password"
+                  Placeholder="Passwod"
+                  v-model="password"
+                  :class="{ error: $v.password.$error }"
+                  @change="$v.password.$touch()"
+                )
+                .error(v-if="!$v.password.required") Password is required.
+                .error(v-if="!$v.password.minLength")
+                  | Password must have at least {{ $v.password.$params.minLength.min }} letters.
+              .buttons-list
+                button.button.button--round.button-primary(
+                  type="submit"
+                  :disabled="submitStatus === 'PENDING'"
+                ) Login
+              .buttons-list.buttons-list--info
+                p.typo__p(v-if="submitStatus === 'OK'") Thanks for your submission!
+                p.typo__p(v-if="submitStatus === 'ERROR'") Please fill the form correctly.
+                p.typo__p(v-if="submitStatus === 'PENDING'") Sending...
+              .buttons-list.buttons-list--info
+                span Need Registration? 
+                  router-link(to="registration") Enter Here
+
 </template>
 
 <script>
+import { required, email, minLength } from 'vuelidate/lib/validators'
+
 export default {
   data() {
     return {
-
+      email: '',
+      password: '',
+      submitStatus: null
+    }
+  },
+  validations: {
+    email: {
+      required,
+      email
+    },
+    password: {
+      required,
+      minLength: minLength(6)
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        this.submitStatus = 'ERROR'
+      } else {
+        console.log('login!')
+        const user = {
+          email: this.email,
+          password: this.password
+        }
+        console.log(user)
+        // do your submit logic here
+        this.submitStatus = 'PENDING'
+        setTimeout(() => {
+          this.submitStatus = 'OK'
+        }, 500)
+      }
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+.auth
+  display flex
+  &__banner, &__form
+    width 50%
 
+.form-item
+  .error
+    display none
+    margin-bottom 8px
+    font-size 13px
+    color #fc5c65
+  &.errorInput
+    .error
+      display block
+
+input
+  &.error
+    border-color #fc5c65
+
+.buttons-list
+  text-align right
+  margin-bottom 20px
+  &--info
+    text-align center
+    &:last-child
+      margin-bottom 0
+
+a
+  color #444ce0
 </style>
--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -2,19 +2,133 @@
   .content-wrapper
     section
       .container
-        h1.ui-title-1 Registration
+        .auth
+          .auth__banner
+            h1.ui-title-2 Hellow Banner
+          .auth__form
+            h2.ui-title-2 Registration
+            form(@submit.prevent="onSubmit")
+              .form-item(:class="{ errorInput: $v.email.$error }")
+                input(
+                  type="email"
+                  Placeholder="Email"
+                  v-model="email"
+                  :class="{ error: $v.email.$error }"
+                  @change="$v.email.$touch()"
+                )
+                .error(v-if="!$v.email.required") Field is required
+                .error(v-if="!$v.email.email") Email is not correct
+              .form-item(:class="{ errorInput: $v.password.$error }")
+                input(
+                  type="password"
+                  Placeholder="Passwod"
+                  v-model="password"
+                  :class="{ error: $v.password.$error }"
+                  @change="$v.password.$touch()"
+                )
+                .error(v-if="!$v.password.required") Password is required.
+                .error(v-if="!$v.password.minLength")
+                  | Password must have at least {{ $v.password.$params.minLength.min }} letters.
+              .form-item(:class="{ errorInput: $v.repeatPassword.$error }")
+                input(
+                  type="password"
+                  Placeholder="Repeat Password"
+                  v-model="repeatPassword"
+                  :class="{ error: $v.repeatPassword.$error }"
+                  @change="$v.repeatPassword.$touch()"
+                )
+                .error(v-if="!$v.repeatPassword.sameAsPassword") Passwords must be identical.
+              .buttons-list
+                button.button.button--round.button-primary(
+                  type="submit"
+                  :disabled="submitStatus === 'PENDING'"
+                ) Registration
+              .buttons-list.buttons-list--info
+                p.typo__p(v-if="submitStatus === 'OK'") Thanks for your submission!
+                p.typo__p(v-if="submitStatus === 'ERROR'") Please fill the form correctly.
+                p.typo__p(v-if="submitStatus === 'PENDING'") Sending...
+              .buttons-list.buttons-list--info
+                span Do you have account? 
+                  router-link(to="login") Enter Here
+
 </template>
 
 <script>
+import { required, email, minLength, sameAs } from 'vuelidate/lib/validators'
+
 export default {
   data() {
     return {
-
+      email: '',
+      password: '',
+      repeatPassword: '',
+      submitStatus: null
+    }
+  },
+  validations: {
+    email: {
+      required,
+      email
+    },
+    password: {
+      required,
+      minLength: minLength(6)
+    },
+    repeatPassword: {
+      sameAsPassword: sameAs('password')
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        this.submitStatus = 'ERROR'
+      } else {
+        console.log('submit!')
+        const user = {
+          email: this.email,
+          password: this.password
+        }
+        console.log(user)
+        // do your submit logic here
+        this.submitStatus = 'PENDING'
+        setTimeout(() => {
+          this.submitStatus = 'OK'
+        }, 500)
+      }
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+.auth
+  display flex
+  &__banner, &__form
+    width 50%
 
+.form-item
+  .error
+    display none
+    margin-bottom 8px
+    font-size 13px
+    color #fc5c65
+  &.errorInput
+    .error
+      display block
+
+input
+  &.error
+    border-color #fc5c65
+
+.buttons-list
+  text-align right
+  margin-bottom 20px
+  &--info
+    text-align center
+    &:last-child
+      margin-bottom 0
+
+a
+  color #444ce0
 </style>
